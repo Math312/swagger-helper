7f275d7dfadf847477e88e22a16fd5a8b03fb1d7
math312 <894688078@qq.com>
2020-10-19 11:13:00 +0800

    完善Apioperation解析功能

--- src/main/java/com/sharedaka/parser/annotation/swagger/ApiOperationParser.java
+++ src/main/java/com/sharedaka/parser/annotation/swagger/ApiOperationParser.java
@@ -1,7 +1,14 @@
 package com.sharedaka.parser.annotation.swagger;
 
 import com.intellij.psi.PsiAnnotation;
+import com.intellij.psi.PsiAnnotationMemberValue;
+import com.intellij.psi.PsiNameValuePair;
+import com.sharedaka.entity.annotation.swagger.ApiOperationEntity;
 import com.sharedaka.parser.annotation.AbstractAnnotationParser;
+import com.sharedaka.utils.StringUtil;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.sharedaka.constant.swagger.SwaggerAnnotations.SWAGGER_API_OPERATION_ANNOTATION_NAME;
 
@@ -14,6 +21,47 @@ public class ApiOperationParser extends AbstractAnnotationParser {
 
     @Override
     protected Object doParse(PsiAnnotation psiAnnotation) {
-        return null;
+        PsiNameValuePair[] attributes = psiAnnotation.getParameterList().getAttributes();
+        Map<String, PsiAnnotationMemberValue> attributeMap = new HashMap<>();
+        for (PsiNameValuePair attribute : attributes) {
+            if (attribute.getValue() != null) {
+                attributeMap.put(attribute.getAttributeName(), attribute.getValue());
+            }
+        }
+        return mapToAnnotationEntity(attributeMap);
+    }
+
+    private Object mapToAnnotationEntity(Map<String, PsiAnnotationMemberValue> attributeMap) {
+        ApiOperationEntity apiOperationEntity = new ApiOperationEntity();
+        if (attributeMap.containsKey("value")) {
+            apiOperationEntity.setValue(StringUtil.removeHeadAndTailQuotationMarks(attributeMap.get("value").getText()));
+        }
+        if (attributeMap.containsKey("notes")) {
+            apiOperationEntity.setNotes(StringUtil.removeHeadAndTailQuotationMarks(attributeMap.get("notes").getText()));
+        }
+        if (attributeMap.containsKey("response")) {
+            apiOperationEntity.setResponse(attributeMap.get("response").getText());
+        }
+        if (attributeMap.containsKey("httpMethod")) {
+            apiOperationEntity.setHttpMethod(StringUtil.removeHeadAndTailQuotationMarks(attributeMap.get("httpMethod").getText()));
+        }
+        if (attributeMap.containsKey("consumes")) {
+            apiOperationEntity.setConsumes(StringUtil.removeHeadAndTailQuotationMarks(attributeMap.get("consumes").getText()));
+        }
+        if (attributeMap.containsKey("tags")) {
+            String content = attributeMap.get("tags").getText();
+            content = StringUtil.removeSpace(content);
+            content = content.substring(1, content.length() - 1);
+            if (content.equals("")) {
+                apiOperationEntity.setTags(new String[]{});
+            } else {
+                String[] contents = content.split(",");
+                for (int i = 0; i < contents.length; i++) {
+                    contents[i] = StringUtil.removeHeadAndTailQuotationMarks(contents[i]);
+                }
+                apiOperationEntity.setTags(contents);
+            }
+        }
+        return apiOperationEntity;
     }
 }
