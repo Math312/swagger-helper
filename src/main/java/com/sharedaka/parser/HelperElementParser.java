ab18296f8854e08bf9ee6f1e26952abdc834f711
math312 <894688078@qq.com>
2020-11-12 20:40:54 +0800

    集成Spring容器支持

--- src/main/java/com/sharedaka/parser/HelperElementParser.java
+++ src/main/java/com/sharedaka/parser/HelperElementParser.java
@@ -2,6 +2,7 @@ package com.sharedaka.parser;
 
 import com.intellij.psi.*;
 import com.intellij.psi.util.MethodSignature;
+import com.intellij.spring.CommonSpringModel;
 import com.intellij.spring.model.SpringBeanPointer;
 import com.intellij.spring.model.SpringModelSearchParameters;
 import com.intellij.spring.model.utils.SpringModelSearchers;
@@ -40,14 +41,17 @@ public class HelperElementParser extends JavaElementVisitor {
         PsiClass abstractClass = psiMethod.getContainingClass();
         if (abstractClass != null) {
             MethodSignature methodSignature = psiMethod.getSignature(PsiSubstitutor.EMPTY);
-            Collection<SpringBeanPointer> springBeanPointerList = SpringModelSearchers.findBeans(SwaggerHelperApplicationManager.getInstance(psiMethod.getProject()).getCommonSpringModel(), SpringModelSearchParameters.byClass(abstractClass));
-            if (springBeanPointerList.size() == 1) {
-                for (SpringBeanPointer springBeanPointer : springBeanPointerList) {
-                    PsiClass psiClass = springBeanPointer.getBeanClass();
-                    PsiMethod[] psiMethods = psiClass.getMethods();
-                    for (PsiMethod methodImpl : psiMethods) {
-                        if (methodImpl.getHierarchicalMethodSignature().equals(methodSignature)) {
-                            return methodImpl;
+            CommonSpringModel springModel = SwaggerHelperApplicationManager.getInstance(psiMethod.getProject()).getCommonSpringModel();
+            if (springModel != null) {
+                Collection<SpringBeanPointer> springBeanPointerList = SpringModelSearchers.findBeans(springModel, SpringModelSearchParameters.byClass(abstractClass));
+                if (springBeanPointerList.size() == 1) {
+                    for (SpringBeanPointer springBeanPointer : springBeanPointerList) {
+                        PsiClass psiClass = springBeanPointer.getBeanClass();
+                        PsiMethod[] psiMethods = psiClass.getMethods();
+                        for (PsiMethod methodImpl : psiMethods) {
+                            if (methodImpl.getHierarchicalMethodSignature().equals(methodSignature)) {
+                                return methodImpl;
+                            }
                         }
                     }
                 }
