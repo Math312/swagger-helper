59ada9ef55cb0911a14dda68a5a572f5abda3788
math312 <894688078@qq.com>
2021-02-05 15:16:33 +0800

    完善文档

--- src/main/java/com/sharedaka/dispatcher/SwaggerActionDispatcher.java
+++ src/main/java/com/sharedaka/dispatcher/SwaggerActionDispatcher.java
@@ -13,6 +13,13 @@ import com.sharedaka.processor.business.SwaggerApiMethodProcessor;
 import com.sharedaka.processor.business.SwaggerApiModelProcessor;
 import com.sharedaka.utils.PsiElementUtil;
 
+/**
+ * Swagger-Helper由一个按钮作为统一入口
+ * 根据用户光标所在位置进行进行不同的处理
+ * 因此需要一个分发器进行功能判断
+ * 该类负责分发功能
+ *
+ * @author math312*/
 public class SwaggerActionDispatcher {
 
     public boolean support(AnActionEvent actionEvent) {
